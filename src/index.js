a2ad36456e6549bd4f281b6fbc2346264ad38abb
Marcus Larsson <marcuslarsson_92@hotmail.com>
2016-04-25 02:00:06 +0200

    added files

---
+++ src/index.js
@@ -0,0 +1,8 @@
+"use strict";
+
+import React from 'react';
+import {render} from 'react-dom';
+import App from './components/App.jsx';
+import './main.css';
+
+render(<App />, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'eval',
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    './src/index.js'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    loaders: [{
+      test: [
+        /\.js$/,
+        /\.jsx$/
+      ],
+      loaders: ['react-hot', 'babel'],
+      include: path.join(__dirname, 'src')
+    }, {
+      test: /\.css$/,
+      loader: 'style!css'
+    }, {
+      test: /\.(jpe?g|png|gif|svg)$/i,
+      loaders: [
+        'url?limit=8192',
+        'img'
+      ]
+    }]
+  }
+};
