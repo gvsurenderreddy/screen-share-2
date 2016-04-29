8e52f5129c8612cde4a6d36cac4b6ee5fb864909
Marcus Larsson <marcuslarsson_92@hotmail.com>
2016-04-29 16:49:11 +0200

    added state to the main jsx component

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -2,6 +2,28 @@
 
 import React from 'react';
 
-export default ({}) => (
-  <div>test</div>
-);
+export default React.createClass({
+  getInitialState() {
+    return { open: false };
+  },
+  componentDidMount() {
+
+  },
+  handleClick() {
+    this.setState({open: !this.state.open})
+  },
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>
+          test
+        </button>
+        {
+          this.state.open ?
+          <div>open</div> :
+          <div>closed</div>
+        }
+      </div>
+    );
+  }
+});
